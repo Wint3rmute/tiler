e52dddc8181e22fcae15748f7a78f72ad2253bbf
wint3rmute <mateusz.baczek1998@gmail.com>
2020-04-15 18:53:15 +0200

    minor style fix

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let num_of_windows = windows.len();
     let settings = size_settings::get_size_settings();
 
-    match num_of_windows {
+    match windows.len() {
         1 => layout_1_window(&mut windows[0]),
         2 => layout_2_windows(&mut windows, settings),
         3 => layout_3_windows(&mut windows),
